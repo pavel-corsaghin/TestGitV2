c21d92961349757eb60ec4d5dbe5fc71db194454
Nguyễn Mạnh Hưng <v.hungnm24@vinid.net>
2020-10-08 19:17:44 +0700

    Init git

---
+++ app/src/main/java/vn/codefresher/translate/MainActivity.java
@@ -0,0 +1,30 @@
+package vn.codefresher.translate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showWelcomeToast();
+    }
+
+    private void showWelcomeToast(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Context context = MainActivity.this;
+                Toast.makeText(context, context.getString(R.string.welcome_text), Toast.LENGTH_LONG)
+                        .show();
+            }
+        }, 5000);
+    }
+}
